af6920ae0e23fde23e0ec984091c4a0c79559493
bebrazavr52 <161696433+bebrazavr52@users.noreply.github.com>
2024-04-02 10:15:35 +0300

    26.03.py

--- 26.03.py
+++ 26.03.py
@@ -96,3 +96,29 @@ for row in matrix:
     if d > b:
         b = d 
 print (b)
+# Задача 35
+import random  
+Result=0
+a=[]
+def creatArray():
+    r = 0
+    x = 2
+    y = 2
+    array = []
+    for i in range(x):
+        array.append([])
+        for j in range(y):
+            array[i].append(random.randint(-10,10))
+            r += 1  
+    return array
+matrix=creatArray()
+print(matrix)
+result=[]
+for i in matrix:
+    for e in i:
+        if e > 0:
+            Result += e 
+    a.append(Result)
+    Result=0
+            
+print(a)
